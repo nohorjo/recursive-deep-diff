425af93e39c2955f85f3f444e721f941ba623d12
Muhammed Haque <m.haque@livelinktechnology.net>
2018-10-03 09:00:15 +0100

    Fixed for new and unchanged object props

--- index.js
+++ index.js
@@ -2,27 +2,27 @@ module.exports = function diff(first, second) {
     if (!first || !second) throw 'first and second cannot be null or undefined';
     if (first.constructor != second.constructor) throw 'Object type mismatch';
     
-    const diffs = new first.constructor;
+    const diffs = JSON.parse(JSON.stringify(second));
 
-    switch (second.constructor) {
+    switch (first.constructor) {
         case Array:
-            second.forEach((value, k) => step(value, k, true));
+            first.forEach((value, k) => step(value, k, true));
             break;
         case Object:
-            Object.entries(second).forEach(([k, value]) => step(value, k));
+            Object.entries(first).forEach(([k, value]) => step(value, k));
             break;
         default: throw 'Can only diff Objects or Arrays';
     }
     
-    function step(value, k, isArrayElement = false) {
-        const existing = first[k];
+    function step(existing, k, isArrayElement = false) {
+        const value = second[k];
         if (existing === undefined && value !== undefined) {
             diffs[k] = value;
         } else if (existing !== undefined && value === undefined) {
-            // FIXME not working with objects
             diffs[k] = undefined;
         } else if (existing == value) {
             if (isArrayElement) diffs[k] = undefined;
+            else delete diffs[k];
         } else if (
             existing.constructor != value.constructor
             || (existing.constructor != Object && existing.constructor != Array)
